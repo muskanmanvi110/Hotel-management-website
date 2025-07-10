f4d9a357f4343d3331507a544150ff16eb316dd2
muskanmanvi110 <144522620+muskanmanvi110@users.noreply.github.com>
2025-07-11 02:56:07 +0530

    Update PhotoRetrievalException.java

--- Backend/src/main/java/com/dailycodework/lakesidehotel/exception/PhotoRetrievalException.java
+++ Backend/src/main/java/com/dailycodework/lakesidehotel/exception/PhotoRetrievalException.java
@@ -1,8 +1,5 @@
 package com.dailycodework.lakesidehotel.exception;
 
-/**
- * @author Simpson Alfred
- */
 
 public class PhotoRetrievalException extends RuntimeException {
     public PhotoRetrievalException(String message) {
